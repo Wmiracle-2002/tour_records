67f3521905c3d52e4fed802f9c0a3f85fa6a230a
Zhanghaolong Chen <573703718@qq.com>
2026-09-23 23:34:39 +0800

    fix: anchor chat input above keyboard

--- app/src/main/java/com/miracle/footmarks/ui/screen/smartplanning/SmartPlanningScreen.kt
+++ app/src/main/java/com/miracle/footmarks/ui/screen/smartplanning/SmartPlanningScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -96,6 +97,7 @@ fun SmartPlanningContent(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .imePadding()
         ) {
             LazyColumn(
                 state = listState,
